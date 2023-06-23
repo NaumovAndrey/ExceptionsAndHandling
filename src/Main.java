c6c2ad1beeb186781eb17f2c7d108dda4af8f759
andrey <naumov.a.n@icloud.com>
2023-06-24 00:53:59 +0700

    seminar 1 main

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        //SeminarOne.taskOne(); // Блок catch
+        //SeminarOne.taskTwo(); // Более одного блока catch (выполняются все операции, программа не останавливается)
+        //SeminarOne.taskThree(); // Вложенный блок catch (программа остановится, как только сработает внешний блок)
+
+        /* Проверка на ошибку прописывается не в методе
+        try {
+            SeminarOne.taskFour();
+        }
+        catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Ошибка");
+            System.out.println(ex);
+            System.out.println("Стек вызовов");
+            ex.printStackTrace();
+        }
+        System.out.println("После оператора catch"); */
+
+        // homework seminar 1
+
+    }
+}
